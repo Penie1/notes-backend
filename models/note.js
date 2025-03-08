6d2c7d35afb41d0396c8ea72116f2773ce59b9c5
Peniel Bekele <142409326+Penie1@users.noreply.github.com>
2025-03-08 16:58:01 +0300

    Implement error handling and validation for notes API

--- models/note.js
+++ models/note.js
@@ -16,14 +16,18 @@ mongoose
   });
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
   important: Boolean,
 });
 
 noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
-    delete returnedObject.id;
+    delete returnedObject._id;
     delete returnedObject.__v;
   },
 });
